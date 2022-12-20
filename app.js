75a8f3500358f006667d7c54f1972cdc5a5e3b60
willywin99 <willywinsianturi@gmail.com>
2022-12-20 23:31:58 +0700

    Challenge 12 Completed

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ app.use(express.static("public"));
 let posts = [];
 
 app.get('/', function(req, res) {
-  res.render('home', {startingContent: homeStartingContent});
-  console.log(posts);
+  res.render('home', {
+    startingContent: homeStartingContent,
+    posts: posts
+  });
 });
 
 app.get('/about', function(req, res) {
